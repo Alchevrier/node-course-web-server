012a2af569fd5d8ad2833fbabc3bcc5165909498
Alex Chevrier <chevrier.alex@gmail.com>
2017-12-12 06:52:27 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log.')
+        }
+    });
+    next();
+});
+
+// var isUnderMaintenance = true;
+
+// app.use((req, res, next) => {
+//     if(isUnderMaintenance) {
+//         res.render('maintenance.hbs');
+//     }
+//     else {
+//         next();
+//     }
+// });
+
+app.use(express.static(`${__dirname}/public`));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name: 'Andrew',
+    //     likes: [
+    //         'Reading',
+    //         'Biking',
+    //         'Cooking',
+    //         'Baking'
+    //     ]
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Welcome Page',
+        welcomeMessage: 'Welcome to my website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Page not found!'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
